c9fdfb4062ffa32d1d6578844ee69f78c740924b
juanchope574 <gonzalezjuandavid574624@gmail.com>
2026-05-25 09:36:29 -0600

    Actualizacion del proyecto

---
+++ Assets/scripts/IceBallDamage.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class IceBallDamage : MonoBehaviour
+{
+    public int damage = 20;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HealthSystem health = collision.gameObject.GetComponent<HealthSystem>();
+
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                Debug.Log("La bola de hielo hizo daño al jugador");
+            }
+        }
+    }
+}
